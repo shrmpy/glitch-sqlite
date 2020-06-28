4620aee009047b81fd111d129fe2fd7f31a6be2b
Shrimp Research <shrmpy@users.noreply.github.com>
2020-06-28 04:19:18 -0700

    start glitch files

---
+++ README.md
@@ -0,0 +1,35 @@
+Hello Sqlite
+=================
+
+Hello world sqlite on glitch.com.
+
+
+Your Project
+------------
+
+### ← README.md
+
+That's this file, where you can tell people what your cool website does and how you built it.
+
+### ← index.html
+
+Where you'll write the content of your website. 
+
+### ← style.css
+
+CSS files add styling rules to your content.
+
+
+Credits
+------------
+
+[Sequelize Sqlite](https://glitch.com/~sqlite3-db)
+
+[better-sqlite3](https://github.com/JoshuaWise/better-sqlite3)
+
+
+
+Made by [Glitch](https://glitch.com/)
+-------------------
+
+\ ゜o゜)ノ
---
+++ server.js
@@ -0,0 +1,34 @@
+const db = require('./db')
+const express = require('express')
+
+// database clean-up
+process.on('exit', () => db.close())
+process.on('SIGHUP', () => process.exit(128 + 1))
+process.on('SIGINT', () => process.exit(128 + 2))
+process.on('SIGTERM', () => process.exit(128 + 15))
+
+const app = express()
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+	response.sendFile(__dirname + '/views/index.html')
+})
+
+app.get("/favorites", (request, response) => {
+  let favorites=[]
+  db.findAll().then((favs) => { // find all entries in the DB tables
+    favs.forEach((fav) => {
+      favorites.push([fav.broadcaster,fav.hls]) // adds their info to the array
+    })
+    response.send(favorites)
+  })
+})
+
+
+
+const listener = app.listen(process.env.PORT, () => {
+	console.log('Your app is listening on port ' + listener.address().port)
+})
