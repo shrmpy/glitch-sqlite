f81fd23f1167da07b3757d7e87eee06f07f1771e
Shrimp Research <shrmpy@users.noreply.github.com>
2020-06-28 04:28:40 -0700

    db/dml.sql populate data

---
+++ db/dml.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO Favorite (broadcaster, quality, hls)
+       VALUES
+       (
+         'gamesdonequick'
+       , '480p'
+       , 'https://video-weaver.sea01.hls.ttvnw.net/v1/playlist/CvgDcC9-58eU0lkorJpeoVEyadeoqD90M10pJtwzttt50kzUhTjOs9TGo9FB7aCa6_jkCtiWLCQ51tYGWbJo3NYBJm3VgYpl4dmBQq84R8N_XJISYEb5H92fDmQMrTT26vI9IoTHFJP01bJ9m0yDftuYN9umvPwRbzKqwIMiiFKrojzfTtcQeNEfI2Id25sdvgCs_M291iLnrRCWaXevYq_mZ6I_OVOSb95OCMK2UvzCZ64uNOjSnvm_iV2V_ZDj7mkdMc08Y0PKKcsVU-FdUzNGgW71K2dMFNDiRFuGezXQxbIhcwc6SJmQ_u0i-HFOPL7x2kBs9JmTs9OnLeVdWnd0UO62Cv8YUAU722pcnyOyN092eNAnbAroX8B2uBKqKEBmKXFwZU3FeAfxcZ87tc0PkEYD_kGr7xyIrhb1fuuCaVElFMLxpGrQ02FyeCEmNO7DJMHfFtqJBJc-2VRAltXyXJduqHGUlvZfHo-ubuvLnMU2bT_m0WxIEQw9s8cD_agz49WOmvAkrBc4OkdpiqIMVr2RgBAQ2frnyA6S4DLztxdQRWCEv1onWU90gShY6NR5lNZX1TWZYo690q6vWQrGzP8_TYorrnWNP-OqK2fLuEX0NCNQF30rEJS87NLNn_yZmoeEcVBFqMY1QmI7l8X0vegRl_FeneXOEhADkl4MMjcDkB73SWrxmGbcGgyVQYG2uZoi1F_1bg0.m3u8'
+       );
+
+INSERT INTO Favorite (broadcaster, quality, hls)
+       VALUES
+       (
+         'feliciaday'
+       , '480p'
+       ,'https://video-weaver.sea01.hls.ttvnw.net/v1/playlist/CrYDoBbNtXZWKTNqV0FsTqIChnHl015noS5dlrNW42xo-LDrMT4RIivDZquelx0-5Bi2rs_ZeKFzKQgyu4Gy_jNyJlQtoJv7MV5bLOw8broon3_TAghlKWmok6PDumanl2tr8eOz1zZBpjpb0BQ7hzsIY6k5QPWpYqannae77Mjm7ySerCz-ik0MlH2IjKMYJgO6dcGxT7Ovk7S05CmjdDtNmv5Z-An7R5RRP2E6VaU5mWIEfOONXmuQ56-y60ksNesMvJA8Dq6l8JL-fAeiSnHSQBFRkieGdi_nJ1ZOQ9Sj4xR3Ntpu4zmY4-HGuDYqWzzE0Dv8Mvt0f4FxqPbGH_zv8vNhrrts0xbTgABB-M69FQorC2J3cCkcOW6-KE5yZx1IKkRLWQ5MkPC8JD76ZFkvKlN5BOUhCApIcOjFVNxBtHUUDwz6Xov_FmooWKzpQskDQFOjE2HiA0QCDWyzG57NWGmg5VzBYyBXhH7zqQxttpDLligGoKdwk_HBB46UIJg0Au9DBmFoy8NfxkbjVh6_QXpF_BlRLjQCktBcE41HpybEHJupBfmbNP5J557T_57ElqvpXwsFEhDhnBqfRhTKCxm1Hiq8KTJMGgyzBL9Asg-ZyNVjdd4.m3u8'
+       );
+
+INSERT INTO Favorite (broadcaster, quality, hls)
+       VALUES
+       (
+         'jessecox'
+       , '480p'
+       ,'https://video-weaver.sea01.hls.ttvnw.net/v1/playlist/CuAD8mFXiCUtROTuSClWpPP7r57hmtkjq26297SbnoO6OoaeTotY-12Ww4LMzSLGJ9iQJgUc5TQQ0mY5Nj6Iem7i_nPy-5hzD7MMG7vg6kYlLWApCtDSPZ5cnIAkfuTgfDSNqpXuGcFjwsQ4mXMlu7WOplmWHhGEghu6oLMUJG2587GyddoZyfhM0_nAmOwTPQG1b-znPdhFkC1uuUF9qYcPRUZDAXpgciJhv6EuKtN3bZ1JSqZuNFxa7cjMQJO5Ps3dkpZBwsCRyHxD0qiIXASFxVkPMtSKPd1TxRRe8VIpMnvW0Kx3N6Y29Qu-uUORgSQ2bcksNCcTOKzvNC4lb6qE17WXo3FWiOyLSX-qcy7aEjtS85dPdmi4KEQY_2KSS91FhiM8Vdwi8KsXidKoQpei6PqsPHfu3d7P1ZNY6E_USva8Men14wk5JY8hS3ueNnEAPJU485ow6gqm9m6dfsR8skg-kPEwA8h5nBU5LiCv-QUQ-C4EHApb_m0Ux4z-5g0ZjsijgplV71FhdcZAjWqTcqwB-Dd4bCft35ouQf2d14-iqRsQqoXIC8P4hmg6ngn7QTWD2BD9xnigFvqvRY69dIQL5r_y-pWbd3od7KShEnrYo-RhB9JQ7fLPrkqPpu8oEhCQxS2tE0Np0K2bQSjC2_cyGgzGM-WXlJBQrl91b9k.m3u8'
+       );
+
