e6c027dc82813bb0fcd34b83009598e0d1e0e9dd
Shrimp Research <shrmpy@users.noreply.github.com>
2020-06-28 23:42:26 -0700

    expose db.reset for /reset route

--- db/index.js
+++ db/index.js
@@ -3,20 +3,23 @@ const Database = require('better-sqlite3')
 const db = new Database(':memory:', { verbose: console.log })
 // foreign key relationships
 db.pragma('foreign_keys = ON')
-// schema
-const ddl = db.prepare(`CREATE TABLE IF NOT EXISTS Favorite (
-        id integer NOT NULL PRIMARY KEY,
-        broadcaster text NOT NULL UNIQUE,
-        quality text,
-        hls text
-    );`
-)
-ddl.run()
-// populate table with default data
-console.log("DEBUG relative dir: ", process.cwd())
-const dml = fs.readFileSync('./db/dml.sql', 'utf8')
-db.exec(dml)
 
+function reschema() {
+	const ddl = db.prepare(`CREATE TABLE IF NOT EXISTS Favorite (
+			id integer NOT NULL PRIMARY KEY,
+			broadcaster text NOT NULL UNIQUE,
+			quality text,
+			hls text
+		);`
+	)
+	ddl.run()
+	// populate table with default data
+	console.log("DEBUG relative dir: ", process.cwd())
+	const dml = fs.readFileSync('./db/dml.sql', 'utf8')
+	db.exec(dml)
+}
+
+reschema()
 
 module.exports = {
 	close: () => { return db.close() },
@@ -24,5 +27,10 @@ module.exports = {
 		return db
 			.prepare('SELECT * FROM Favorite')
 			.all()
+	},
+	reset: () => {
+		const truncate = db.prepare(`DROP TABLE Favorite;`)
+		truncate.run()
+		reschema()
 	},
 }
